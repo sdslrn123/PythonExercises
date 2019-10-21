86d0c259876e1e2e7c44f921a7df59eaa1068b97
sdslrn123 <46789069+sdslrn123@users.noreply.github.com>
2019-10-21 21:09:28 +0100

    Add files via upload

---
+++ storygenerator.py
@@ -0,0 +1,8 @@
+#StoryGenerator
+
+mainCharacter=input("Name of main character: ")
+pastVerb=input("Name of past verb: ")
+sentenceObject=input("Name of object: ")
+filler="on the"
+story=(mainCharacter+' '+pastVerb+' '+filler+' '+sentenceObject)
+print(story)
